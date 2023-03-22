0942f438ec1dca52390efdcc37f024df717b8af7
abdussamadsaiful7 <samadsaifullah11@gmail.com>
2023-03-23 04:19:26 +0600

    added component and fetch url

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
@@ -7,8 +7,38 @@ function App() {
       <District name="Noakhali" specialty="Division"></District>
       <District name="Bi-Bariya" specialty="Jodda Akbar"></District>
       <District name="Cumilla" specialty="Moynamoti" ></District>
+      <LoadPosts></LoadPosts>
     </div>
   );
+}
+
+function LoadPosts(){
+  const [Posts, setPosts] =useState([]);
+
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => setPosts(data))
+  }, [])
+
+  return(
+    <div>
+      <h1>Posts: {Posts.length}</h1>
+      {
+        Posts.map(post => <Post title={post.title} body={post.body}></Post>)
+      }
+    </div>
+
+  )
+}
+
+function Post(props){
+  return(
+    <div style={{backgroundColor: 'lightgray', margin:'20px', border:'2px solid salmon'}}>
+      <h3>Title: {props.title}</h3>
+      <p>Body : {props.body}</p>
+    </div>
+  )
 }
 
 const districtStyle ={
